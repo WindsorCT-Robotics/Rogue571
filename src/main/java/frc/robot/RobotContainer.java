c5723d1238d6080e3aa9a32ee7b49469cf2da4ab
programming571@gmail.com <programming571@gmail.com>
2020-02-01 16:57:18 -0500

    Create DriveCommand skeleton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,8 @@ import frc.robot.subsystems.Color;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ConveyerMaster;
 import frc.robot.commands.SpinToColor;
 import frc.robot.subsystems.Climb;
@@ -19,8 +21,9 @@ import frc.robot.subsystems.Leveling;
 import frc.robot.subsystems.PowerCollection;
 
 /**
- * This class takes the place of much of the old robot class, and entirely replaces OI.java 
- * It initializes the joysticks, their buttons, and their axis, and it binds them to commands.
+ * This class takes the place of much of the old robot class, and entirely
+ * replaces OI.java It initializes the joysticks, their buttons, and their axis,
+ * and it binds them to commands.
  */
 public class RobotContainer {
     // initializing robot subsytems
@@ -29,39 +32,39 @@ public class RobotContainer {
     private final Drive drive = new Drive();
     private final Leveling level = new Leveling();
     private final PowerCollection powerCollection = new PowerCollection();
-    //initialize joystick and off-brand XBox conroller from Chinese Walmart
-    private final Joystick drivestick = new Joystick(0);
-    private final XboxController opstick = new XboxController(1);
+    // initialize joystick and off-brand XBox conroller from Chinese Walmart
+    private final Joystick driveStick = new Joystick(0);
+    private final XboxController opStick = new XboxController(1);
+    
 
-    //TODO: put in commandbase
+    // TODO: put in commandbase
 
     public RobotContainer() {
-        //TODO: SmartDashBoard
+        // TODO: SmartDashBoard
+        Shuffleboard.putData("JoystickX", driveStick.getX());
 
         configureButtonBindings();
     }
 
     private void configureButtonBindings() {
-        //xbox buttons
-        final JoystickButton green = new JoystickButton(opstick, 1);
-        final JoystickButton red = new JoystickButton(opstick, 2);
-        final JoystickButton blue = new JoystickButton(opstick, 3);
-        final JoystickButton yellow = new JoystickButton(opstick, 4);
-        //joystick buttons
-        final JoystickButton intake = new JoystickButton(drivestick, 2);
-        final JoystickButton output = new JoystickButton(drivestick, 1);
+        // xbox buttons
+        final JoystickButton green = new JoystickButton(opStick, 1);
+        final JoystickButton red = new JoystickButton(opStick, 2);
+        final JoystickButton blue = new JoystickButton(opStick, 3);
+        final JoystickButton yellow = new JoystickButton(opStick, 4);
+        // joystick buttons
+        final JoystickButton intake = new JoystickButton(driveStick, 2);
+        final JoystickButton output = new JoystickButton(driveStick, 1);
 
-        //TODO: put in color parameters
-        green.whenPressed(new SpinToColor());
-        red.whenPressed(new SpinToColor());
-        blue.whenPressed(new SpinToColor());
-        yellow.whenPressed(new SpinToColor());
-/*
-        intake.whenPressed();
-        output.whenPressed();*/
+
+
+        // TODO: put in color parameters
+        // green.whenPressed(new SpinToColor((byte)1));
+        // red.whenPressed(new SpinToColor((byte)2));
+        // blue.whenPressed(new SpinToColor((byte)3));
+        // yellow.whenPressed(new SpinToColor((byte)4));
+
+        // intake.whenPressed(); output.whenPressed();
     }
-
-    
-    
 
 }
