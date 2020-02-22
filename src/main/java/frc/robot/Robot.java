85a4b24ccac6cf6a3c80549a1d1d81e7249b64c9
programming571@gmail.com <programming571@gmail.com>
2020-02-22 17:13:57 -0500

    Implement joystick controls
    * Joystick controls have been implemented.
    * Input and output values are now displayed on Shuffleboard.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,9 +15,10 @@ import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -52,12 +53,10 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void disabledInit() {
-
     }
 
     @Override
     public void disabledPeriodic() {
-        Scheduler.getInstance().run();
     }
 
     @Override
@@ -73,7 +72,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
     }
 
     @Override
@@ -84,6 +82,7 @@ public class Robot extends TimedRobot {
         // this line or comment it out.
         if (autonomousCommand != null)
             autonomousCommand.cancel();
+        CommandScheduler.getInstance().run();
     }
 
     /**
@@ -91,6 +90,11 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
-        Scheduler.getInstance().run();
     }
+
+    @Override
+    public void robotPeriodic(){
+        CommandScheduler.getInstance().run();
+    }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,14 +13,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AutonomousCommand;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.ShootBalls;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.ColorSubsystem;
+import frc.robot.subsystems.Compressor;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Leveling;
 import frc.robot.subsystems.Power;
-import frc.robot.subsystems.Compressor;
 
 /**
  * This class takes the place of much of the old robot class, and entirely
@@ -29,24 +28,40 @@ import frc.robot.subsystems.Compressor;
  */
 public class RobotContainer {
     // initializing robot subsytems
-    private final Climber climb = new Climber();
-    private final ColorSubsystem color = new ColorSubsystem();
-    private final Drive drive = new Drive();
-    private final Leveling level = new Leveling();
-    private final Conveyor conveyor = new Conveyor();
-    private final Compressor compressor = new Compressor();
-    private final Power power = new Power();
+    private Climber climb;
+    private ColorSubsystem color;
+    private Drive drive;
+    private Leveling level;
+    private Conveyor conveyor;
+    private Compressor compressor;
+    private Power power;
+
+    private boolean climbEnabled = false;
+    private boolean colorEnabled = false;
+    private boolean driveEnabled = true;
+    private boolean levelEnabled = false;
+    private boolean conveyorEnabled = false;
+    private boolean compressorEnabled = false;
+    private boolean powerEnabled = true;
 
     // initialize joystick and off-brand XBox conroller from Chinese Walmart
     private final Joystick driveStick = new Joystick(0);
     private final XboxController opStick = new XboxController(1);
-    private final DriveCommand driveCommand = new DriveCommand(drive, () -> driveStick.getY(), () -> driveStick.getZ());
-    
+    private DriveCommand driveCommand;
+
     // TODO: put in commandbase
 
     public RobotContainer() {
+        createClimbSubsystem();
+        createColorSubsystem();
+        createCompressorSubsystem();
+        createConveyorSubsystem();
         configureButtonBindings();
-        //drive.setDefaultCommand();
+        createDriveSubsystem();
+        drive.setDefaultCommand(driveCommand);
+        createLevelSubsystem();
+        createPowerSubsystem();
+
     }
 
     private void configureButtonBindings() {
@@ -59,18 +74,62 @@ public class RobotContainer {
         final JoystickButton intake = new JoystickButton(driveStick, 2);
         final JoystickButton output = new JoystickButton(driveStick, 1);
 
+        
+
         // TODO: put in color parameters
         // green.whenPressed(new SpinToColor((byte)1));
         // red.whenPressed(new SpinToColor((byte)2));
         // blue.whenPressed(new SpinToColor((byte)3));
         // yellow.whenPressed(new SpinToColor((byte)4));
-        output.whileHeld(new ShootBalls(conveyor, .25));
+        // output.whileHeld(new ShootBalls(conveyor, .25));
         // intake.whenPressed(); output.whenPressed();
     }
-    
-    public Command getAutonomousCommand(){
+
+    public Command getAutonomousCommand() {
         return new AutonomousCommand(drive);
     }
 
+    public void createClimbSubsystem() {
+        if (climbEnabled) {
+            climb = new Climber();
+        }
+    }
+
+    public void createColorSubsystem() {
+        if (colorEnabled) {
+            color = new ColorSubsystem();
+        }
+    }
+
+    public void createCompressorSubsystem() {
+        if (compressorEnabled) {
+            compressor = new Compressor();
+        }
+    }
+
+    public void createConveyorSubsystem() {
+        if (conveyorEnabled) {
+            conveyor = new Conveyor();
+        }
+    }
+
+    public void createDriveSubsystem() {
+        if (driveEnabled) {
+            drive = new Drive();
+            driveCommand = new DriveCommand(drive, () -> driveStick.getY(), () -> driveStick.getZ());
+        }
+    }
+
+    public void createLevelSubsystem() {
+        if (levelEnabled) {
+            level = new Leveling();
+        }
+    }
+
+    public void createPowerSubsystem() {
+        if (powerEnabled) {
+            power = new Power();
+        }
+    }
 
 }
