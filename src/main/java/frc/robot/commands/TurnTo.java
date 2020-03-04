d29e470fe56af087885a5e7fac448b122ae50868
programming571@gmail.com <programming571@gmail.com>
2020-03-03 20:28:42 -0500

    live window telemetry and get p,i,d

--- src/main/java/frc/robot/commands/TurnTo.java
+++ src/main/java/frc/robot/commands/TurnTo.java
@@ -7,10 +7,13 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.Drive;
 
@@ -18,16 +21,19 @@ import frc.robot.subsystems.Drive;
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class TurnTo extends PIDCommand {
+  private final ShuffleboardTab tab = Shuffleboard.getTab("Commands");
+  private final ShuffleboardLayout pidLayout = tab.getLayout("pid", BuiltInLayouts.kList);
 
   /**
    * Creates a new TurnTo.
-   * @param heading  heading on a scale from -180 to 180
-   * @param drive    drive subsystem that must be passed in
+   * 
+   * @param heading heading on a scale from -180 to 180
+   * @param drive   drive subsystem that must be passed in
    */
   public TurnTo(double heading, Drive drive) {
     super(
         // The controller that the command will use
-        new PIDController(.5, 0.0, 0.0),
+        new PIDController(0.5, 0.0, 0.0),
         // This should return the measurement
         drive.navx::getYaw,
         // This should return the setpoint (can also be a constant)
@@ -36,7 +42,7 @@ public class TurnTo extends PIDCommand {
         output -> drive.tankDrive(output, -output));
     addRequirements(drive);
     getController().setTolerance(5);
-    
+    LiveWindow.enableTelemetry(this.getController());
   }
 
   @Override
@@ -48,5 +54,20 @@ public class TurnTo extends PIDCommand {
   @Override
   public boolean isFinished() {
     return getController().atSetpoint();
+  }
+
+  public double getP() {
+    NetworkTableEntry kP = pidLayout.add("kP", 0.5).getEntry();
+    return kP.getDouble(0);
+  }
+
+  public double getI() {
+    NetworkTableEntry kI = pidLayout.add("kI", 0.5).getEntry();
+    return kI.getDouble(0);
+  }
+
+  public double getD() {
+    NetworkTableEntry kD = pidLayout.add("kD", 0.5).getEntry();
+    return kD.getDouble(0);
   }
 }
